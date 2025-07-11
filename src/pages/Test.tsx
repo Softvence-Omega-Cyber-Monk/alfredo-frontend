cdf01df9ae41faf835a04e877f64e784ad4d4363
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-07-11 12:23:02 +0600

    reusable subscribe and button component added

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,11 @@
+import Subscribe from "@/components/reusable/Subscribe";
+
 const Test = () => {
-  return <div className="h-screen">Test</div>;
+  return (
+    <div className="h-screen">
+      <Subscribe />
+    </div>
+  );
 };
 
 export default Test;
