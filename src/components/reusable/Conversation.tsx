5b4dc15dd8240914001df6a6845cf056d1538f5a
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-07-11 17:10:40 +0600

    testimonial

--- src/components/reusable/Conversation.tsx
+++ src/components/reusable/Conversation.tsx
@@ -1,5 +1,30 @@
+import conversationImage from "@/assets/conversation.svg";
+// import CommonWrapper from "@/common/CommonWrapper";
+
 const Conversation = () => {
-  return <div>Conversation</div>;
+  return (
+    <>
+      <div className="w-full h-[220px] bg-primary-gray-bg flex items-center gap-2 rounded-3xl px-7">
+        <div>
+          <img src={conversationImage} alt="" />
+        </div>
+        <div className="flex-1 px-12">
+          <h1 className="text-primary-blue text-[32px] font-semibold">
+            Live Chat Support
+          </h1>
+          <p className="text-basic-dark mt-3 font-extralight">
+            Need help? Start a live chat with our team and get quick, real-time
+            support whenever you need it.
+          </p>
+        </div>
+        <div>
+          <button className="bg-white border border-primary-blue text-primary-blue w-[251px] h-[50px] rounded-full">
+            Start a conversation
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Conversation;
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,9 +1,17 @@
+import CommonWrapper from "@/common/CommonWrapper";
+import Conversation from "@/components/reusable/Conversation";
 import Subscribe from "@/components/reusable/Subscribe";
+import Testimonial from "@/components/reusable/Testimonial";
 
 const Test = () => {
   return (
-    <div className="h-screen">
+    <div className="">
       <Subscribe />
+      <div className="mt-5">
+        <CommonWrapper>
+          <Testimonial />
+        </CommonWrapper>
+      </div>
     </div>
   );
 };
