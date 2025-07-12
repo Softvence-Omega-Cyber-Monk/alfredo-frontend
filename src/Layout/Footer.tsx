e1ccba70d505abf3c9c8c6d1e3e747b3e5fe4886
kazimehedihasan1 <asaha8313@gmail.com>
2025-07-12 08:54:45 +0600

    footer-completed

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -1,4 +1,3 @@
-import footerBg from "../assets/footer/footer-bg.svg";
 import facebook from "../assets/footer/fb.svg";
 import instagram from "../assets/footer/instagram.svg";
 import linkedin from "../assets/footer/linkedin.svg";
@@ -8,28 +7,30 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="pb-10 border border-t-black">
+    <footer className=" border border-t-black ">
       {/* Overlay for better text readability */}
 
-      <div className="max-w-7xl mx-auto py-10">
-        <img src={cityscape} alt="" className="max-w-[1132px] mx-auto" />
+      <div className="max-w-7xl mx-auto pt-10 px-4">
+        <div className="max-w-[1132px] mx-auto -mb-2">
+          <img src={cityscape} alt="" className="w-full mx-auto" />
+        </div>
 
         <div
-          className="relative z-10 border border-black px-6 md:px-10 lg:px-20 py-6 md:py-8 lg:py-10 rounded-xl md:rounded-2xl lg:rounded-[40px]"
+          className="px-6 md:px-10 lg:px-20 py-6 md:py-8 lg:py-10 rounded-xl md:rounded-2xl lg:rounded-[40px] h-auto"
           style={{
-            backgroundImage: `url(${footerBg})`,
+            backgroundImage: `url("/footerBg.svg")`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
           }}
         >
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 lg:gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
             {/* First Column - Takes 2/5 on large screens */}
             <div className="lg:col-span-2">
-              <h3 className="text-2xl font-bold mb-6 text-primary-blue">
-                Logo
-              </h3>
-              <p className="text-dark-2 lg:text-xl leading-relaxed lg:max-w-md">
+              <div className="w-36 mb-4 lg:mb-6">
+                <img src="/logo.svg" alt="" />
+              </div>
+              <p className="text-dark-2 lg:text-xl leading-relaxed lg:max-w-md pr-6">
                 "HomeExchange transformed our family vacations! We've stayed in
                 amazing places without breaking the bank."
               </p>
@@ -37,7 +38,7 @@ const Footer = () => {
 
             {/* Second Column - Quick Links */}
             <div>
-              <h3 className="text-lg lg:text-xl text-primary-blue font-semibold mb-4">
+              <h3 className="text-lg lg:text-xl text-primary-blue font-semibold mb-2 md:mb-4">
                 Pages
               </h3>
               <ul className="space-y-3">
@@ -47,13 +48,13 @@ const Footer = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link to="/" className="font-regular lg:text-xl">
-                    Home
+                  <Link to="/faq" className="font-regular lg:text-xl">
+                    FAQ
                   </Link>
                 </li>
                 <li>
-                  <Link to="/" className="font-regular lg:text-xl">
-                    Home
+                  <Link to="/articles" className="font-regular lg:text-xl">
+                    Article
                   </Link>
                 </li>
               </ul>
@@ -61,23 +62,23 @@ const Footer = () => {
 
             {/* Third Column - Services */}
             <div>
-              <h3 className="text-lg lg:text-xl text-primary-blue font-semibold mb-4">
+              <h3 className="text-lg lg:text-xl text-primary-blue font-semibold mb-2 md:mb-4">
                 Pages
               </h3>
               <ul className="space-y-3">
                 <li>
-                  <Link to="/" className="font-regular lg:text-xl ">
-                    Home
+                  <Link to="/contact" className="font-regular lg:text-xl ">
+                    Contact Us
                   </Link>
                 </li>
                 <li>
-                  <Link to="/" className="font-regular lg:text-xl ">
-                    Home
+                  <Link to="/bonus-program" className="font-regular lg:text-xl ">
+                    Bonus Programs
                   </Link>
                 </li>
                 <li>
-                  <Link to="/" className="font-regular lg:text-xl ">
-                    Home
+                  <Link to="/plans" className="font-regular lg:text-xl ">
+                    Plans
                   </Link>
                 </li>
               </ul>
@@ -85,7 +86,7 @@ const Footer = () => {
 
             {/* Fourth Column - Newsletter */}
             <div>
-              <h3 className="text-lg lg:text-xl text-primary-blue font-semibold mb-4">
+              <h3 className="text-lg lg:text-xl text-primary-blue font-semibold mb-2 md:mb-4">
                 Social Media
               </h3>
               <ul className="space-y-3">
@@ -120,6 +121,11 @@ const Footer = () => {
             </div>
           </div>
         </div>
+      </div>
+      {/* Footer Bottom Section */}
+      <div className="relative text-center py-6 md:py-8 lg:py-10 text-dark-2 font-normal text-base">
+        <div className="absolute inset-0 -z-10 backdrop-blur-[50px] bg-gradient-to-b from-white via-[rgba(255,255,255,0.5)] via-40% to-[rgba(49,116,205,0.4)]"></div>
+        <p>Copyright WP Estate. All Rights Reserved.</p>
       </div>
     </footer>
   );
