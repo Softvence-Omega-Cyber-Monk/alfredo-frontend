2b250de1349af19b92eb5262fc20be3d4bdcedf1
rakibwebdev23 <rh.rakibhasan365@gmail.com>
2025-07-13 16:11:26 +0600

    plans page created

--- src/types/index.ts
+++ src/types/index.ts
@@ -23,3 +23,37 @@ export interface User {
   phone: string;
   message: string;
 };
+
+// Service plan card 
+export interface Plan {
+    title: string;
+    duration: string;
+    price: number;
+    originalPrice?: number;
+    badge?: string;
+    features: string[];
+}
+
+// Plans page 
+export interface MembershipStage {
+  title: string;
+  subtitle: string;
+  completed: boolean;
+}
+
+// Bonus Progamme page 
+export interface Card {
+  title: string;
+  titleSub: string;
+  amount: number;
+  currency?: string;
+  bgColor: string;
+  borderColor: string;
+  img: string;
+  color: string;
+}
+export interface InviteDataItem {
+  headTitle: string;
+  points: string[];
+}
+// -------------------------
