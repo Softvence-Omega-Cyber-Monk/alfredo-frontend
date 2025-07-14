4e4ceaa72388144d7994d17a1266b7accdd46489
rakibwebdev23 <rh.rakibhasan365@gmail.com>
2025-07-14 10:11:36 +0600

    contact, FAQ, bonus plan , plan component fully responsive

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -10,58 +10,74 @@ const FAQ = () => {
   return (
     <div>
       <CommonWrapper>
-        <div className="mt-[64px]">
+        <div className="mt-[64px] max-[767px]:mt-[34px] max-[767px]:text-center">
           <ClientHeading headingText="Frequently ask" spanText="questions" />
-          <p className="text-[24px] font-normal text-basic-dark max-[767px]:text-sm text-center">
-            Explore our most frequently asked questions to understand how home exchange works, how to get started, <br /> and how we ensure a safe and trusted travel experience for all our members.
+          <p className="text-[24px] font-normal text-basic-dark max-[767px]:text-base text-center">
+            Explore our most frequently asked questions to understand how home exchange works, how to get started, <br className="max-[767px]:hidden" />
+            and how we ensure a safe and trusted travel experience for all our members.
           </p>
         </div>
-        <div className="mt-[80px] mb-[140px]">
-          <div className="w-full flex justify-between">
-            <div className="w-1/3">
+
+        <div className="mt-[80px] max-[767px]:mb-[70px] mb-[140px] space-y-[64px] max-[767px]:space-y-[30px]">
+          {/* About Vacanza */}
+          <div className="w-full flex justify-between max-[767px]:flex-col max-[767px]:gap-4">
+            <div className="w-1/3 max-[767px]:w-full">
               <h4 className="text-[24px] font-semibold text-primary-blue">About Vacanza</h4>
             </div>
-            <div className="w-2/3">
+            <div className="w-2/3 max-[767px]:w-full">
               <AccordionComponent items={bonus} />
             </div>
           </div>
-          <div className="w-full flex justify-between my-[64px]">
-            <div className="w-1/3">
+
+          {/* About My Profile */}
+          <div className="w-full flex justify-between max-[767px]:flex-col max-[767px]:gap-4">
+            <div className="w-1/3 max-[767px]:w-full">
               <h4 className="text-[24px] font-semibold text-primary-blue">About my profile</h4>
             </div>
-            <div className="w-2/3">
+            <div className="w-2/3 max-[767px]:w-full">
               <AccordionComponent items={aboutProfile} />
             </div>
           </div>
-          <div className="w-full flex justify-between">
-            <div className="w-1/3">
+
+          {/* About Properties */}
+          <div className="w-full flex justify-between max-[767px]:flex-col max-[767px]:gap-4">
+            <div className="w-1/3 max-[767px]:w-full">
               <h4 className="text-[24px] font-semibold text-primary-blue">About properties</h4>
             </div>
-            <div className="w-2/3">
+            <div className="w-2/3 max-[767px]:w-full">
               <AccordionComponent items={aboutProperties} />
             </div>
           </div>
-          <div className="w-full flex justify-between mt-[64px]">
-            <div className="w-1/3">
+
+          {/* About Program */}
+          <div className="w-full flex justify-between max-[767px]:flex-col max-[767px]:gap-4">
+            <div className="w-1/3 max-[767px]:w-full">
               <h4 className="text-[24px] font-semibold text-primary-blue">About properties</h4>
             </div>
-            <div className="w-2/3">
+            <div className="w-2/3 max-[767px]:w-full">
               <AccordionComponent items={aboutPrgram} />
             </div>
           </div>
         </div>
-        <div>
+
+        {/* Plans Section */}
+        <div className="">
           <ClientHeading headingText="Our" spanText="plans" />
-          <p className="text-[24px] font-normal text-basic-dark max-[767px]:text-sm text-center">
-            Flexible membership options designed to fit every traveler’s needs—<br />
+          <p className="text-[24px] font-normal text-basic-dark max-[767px]:text-base text-center">
+            Flexible membership options designed to fit every traveler’s needs—<br className="max-[767px]:hidden" />
             whether you exchange once a year or every month.
           </p>
         </div>
+
         <div className="mt-[80px]">
           <ServicePlan />
         </div>
       </CommonWrapper>
-      <Testimonial />
+
+      {/* Outside wrapper elements */}
+      <div className="max-[767px]:my-20">
+        <Testimonial />
+      </div>
       <Subscribe />
     </div>
   );
