0a895ca807a979bfe6fe21d4bfdf8fa30358debd
kazimehedihasan1 <asaha8313@gmail.com>
2025-07-15 09:38:48 +0600

    home details updated

--- src/components/home-details/Amenities.tsx
+++ src/components/home-details/Amenities.tsx
@@ -1,7 +1,7 @@
 import AmenityCard from "../reusable/AmenityCard";
 
 interface AmenitySection {
-  icon: React.ElementType; // changed from LucideIcon
+  icon: string; // changed from LucideIcon
   title: string;
   items: string[];
 }
@@ -23,7 +23,7 @@ const Amenities: React.FC<AmenitiesProps> = ({ amenities }) => {
       <h2 className="font-semibold text-primary-blue text-xl md:text-2xl font-regular mb-6">
         Amenities
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
         {amenities.main.map((amenity, index) => (
           <AmenityCard key={index} {...amenity} />
         ))}
@@ -32,7 +32,7 @@ const Amenities: React.FC<AmenitiesProps> = ({ amenities }) => {
       <h2 className="font-semibold text-primary-blue text-xl md:text-2xl font-regular mt-10 mb-6">
         Means of Transport
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
         {amenities.transport.map((amenity, index) => (
           <AmenityCard key={index} {...amenity} />
         ))}
@@ -41,7 +41,7 @@ const Amenities: React.FC<AmenitiesProps> = ({ amenities }) => {
       <h2 className="font-semibold text-primary-blue text-xl md:text-2xl font-regular mt-10 mb-6">
         What's surrounding your home?
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
         {amenities.surrounding.map((amenity, index) => (
           <AmenityCard key={index} {...amenity} />
         ))}
