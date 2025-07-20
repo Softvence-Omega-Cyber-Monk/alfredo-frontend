1bdef31c9c0315dfb1f7bfd2801494ec4503f30e
kazimehedihasan1 <asaha8313@gmail.com>
2025-07-20 16:49:07 +0600

    fixed the multistep form issue and improve responsiveness

--- src/components/onboarding/GetStarted.tsx
+++ src/components/onboarding/GetStarted.tsx
@@ -24,13 +24,13 @@ const GetStarted = () => {
     : "";
 
   return (
-    <div className="w-full  md:py-10 space-y-6 ">
+    <div className="w-full py-6 md:py-10 space-y-6 ">
       {/* Header */}
-      <div className="flex flex-col lg:flex-row justify-between items-center mb-10 w-full gap-4">
-        <div className="w-full lg:w-auto flex-1">
-          <Title title="Get started with Vacaza." />
+      <div className="flex flex-col md:flex-row justify-between items-center mb-10 w-full gap-4">
+        <div className="w-full  flex-1">
+          <Title title="Get started with Vacanza." />
         </div>
-        <div className="w-full lg:w-auto flex justify-end">
+        <div className=" flex justify-end">
           <Button
             variant="secondary"
             className="flex items-center gap-2 px-5 py-3 rounded-lg border border-[#CAD2DB] text-[#3174CD] text-base font-medium hover:bg-gray-100"
@@ -45,33 +45,33 @@ const GetStarted = () => {
 
       <div className="flex flex-col lg:flex-row w-full gap-5">
         <div className="w-full lg:w-1/2">
-          <div className="mt-10">
-            <div className="flex flex-col items-start gap-[26px] p-10 flex-1 rounded-[24px] bg-[#F4F7FC] ">
+          <div className="mt-6 lg:mt-10">
+            <div className="flex flex-col items-start gap-[26px] p-6 md:p-10 flex-1 rounded-[24px] bg-[#F4F7FC] ">
               <div>
-                <h3 className="text-2xl text-primary-blue font-semibold mt-3">
+                <h3 className="text-xl md:text-2xl text-primary-blue font-semibold mt-3">
                   1. Create your home listing
                 </h3>
-                <p className="text-lg text-dark-3 font-regular mt-3 max-w-md">
+                <p className="text-base md:text-lg text-dark-3 font-regular mt-3 max-w-md">
                   Add your property details, photos, and pricing to start
                   attracting guests.
                 </p>
               </div>
 
               <div>
-                <h3 className="text-2xl text-primary-blue font-semibold mt-3">
+                <h3 className="text-xl md:text-2xl text-primary-blue font-semibold mt-3">
                   2. Introduce yourself.
                 </h3>
-                <p className="text-lg text-dark-3 font-regular mt-3 max-w-md">
+                <p className="text-base md:text-lg text-dark-3 font-regular mt-3 max-w-md">
                   Share a bit about who you are to build trust with potential
                   guests.
                 </p>
               </div>
 
               <div>
-                <h3 className="text-2xl text-primary-blue font-semibold mt-3">
+                <h3 className="text-xl md:text-2xl text-primary-blue font-semibold mt-3">
                   3. Become a member for 59 EUR
                 </h3>
-                <p className="text-lg text-dark-3 font-regular mt-3 max-w-md">
+                <p className="text-base md:text-lg text-dark-3 font-regular mt-3 max-w-md">
                   Unlock full platform benefits and connect with a global
                   community..
                 </p>
@@ -184,25 +184,6 @@ const GetStarted = () => {
           </div>
         </div>
       </div>
-
-      {/* <div>
-       
-        <div className="flex flex-col sm:flex-row justify-between items-center gap-4 mt-6">
-          <Button className="w-full sm:w-[125px] h-[52px] px-6 py-2 text-[rgba(128,128,128,0.5)] font-inter text-[18px] font-semibold leading-[156%] border border-[#CAD2DB] rounded-[35px] cursor-pointer">
-            Cancel
-          </Button>
-
-          <div className="flex gap-4 w-full sm:w-auto justify-end">
-            <Button
-              type="submit"
-              className="w-full sm:w-[246px] h-[52px] px-8 py-3 rounded-[35px] bg-[#3174CD] hover:bg-[#215ba8] text-white font-inter text-[18px] font-semibold leading-[156%] flex justify-center items-center  gap-[10px] cursor-pointer"
-            >
-              Continue Address
-              <FaArrowRight className="w-5 h-5" />
-            </Button>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
--- src/components/onboarding/Shared/Title.tsx
+++ src/components/onboarding/Shared/Title.tsx
@@ -5,7 +5,7 @@ type SectionTitleProps = {
 export const Title = ({ title }: SectionTitleProps) => {
   return (
     <div>
-      <h1 className="text-[#3174CD] font-dm-sans text-[40px] leading-[60px] not-italic font-DM-sans">
+      <h1 className="text-[#3174CD] font-dm-sans text-3xl md:text-4xl lg:text-[40px] lg:leading-[60px] not-italic font-DM-sans text-center md:text-left">
         {title}
       </h1>
     </div>
