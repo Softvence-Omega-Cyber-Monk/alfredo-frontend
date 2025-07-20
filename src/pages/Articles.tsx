f809c24df5029859707310e0aedbc91d17aed7a9
kazimehedihasan1 <asaha8313@gmail.com>
2025-07-20 17:01:32 +0600

    minor update

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,21 +1,73 @@
 import CommonWrapper from "@/common/CommonWrapper";
 import { useState } from "react";
-import TopArticle from '../components/articles/TopArticle';
-import ArticleGrid from '../components/articles/ArticleGrid';
-import Testimonial from '@/components/reusable/Testimonial';
+import TopArticle from "../components/articles/TopArticle";
+import ArticleGrid from "../components/articles/ArticleGrid";
+import Testimonial from "@/components/reusable/Testimonial";
 
 const Articles = () => {
   const [activeTab, setActiveTab] = useState(0);
 
   const tabs = [
-    { id: 0, label: "Topic Name 1", content: <div><TopArticle/>
-    <ArticleGrid/>
-    </div> },
-    { id: 1, label: "Topic Name 2", content: <div>Content for Tab 2</div> },
-    { id: 2, label: "Topic Name 3", content: <div>Content for Tab 3</div> },
-    { id: 3, label: "Topic Name 4", content: <div>Content for Tab 4</div> },
-    { id: 4, label: "Topic Name 5", content: <div>Content for Tab 5</div> },
-    { id: 5, label: "Topic Name 6", content: <div>Content for Tab 6</div> },
+    {
+      id: 0,
+      label: "Topic Name 1",
+      content: (
+        <div>
+          <TopArticle />
+          <ArticleGrid />
+        </div>
+      ),
+    },
+    {
+      id: 1,
+      label: "Topic Name 2",
+      content: (
+        <div>
+          <TopArticle />
+          <ArticleGrid />
+        </div>
+      ),
+    },
+    {
+      id: 2,
+      label: "Topic Name 3",
+      content: (
+        <div>
+          <TopArticle />
+          <ArticleGrid />
+        </div>
+      ),
+    },
+    {
+      id: 3,
+      label: "Topic Name 4",
+      content: (
+        <div>
+          <TopArticle />
+          <ArticleGrid />
+        </div>
+      ),
+    },
+    {
+      id: 4,
+      label: "Topic Name 5",
+      content: (
+        <div>
+          <TopArticle />
+          <ArticleGrid />
+        </div>
+      ),
+    },
+    {
+      id: 5,
+      label: "Topic Name 6",
+      content: (
+        <div>
+          <TopArticle />
+          <ArticleGrid />
+        </div>
+      ),
+    },
   ];
   return (
     <div className="">
@@ -51,7 +103,7 @@ const Articles = () => {
           <div className="p-4 mt-10">{tabs[activeTab].content}</div>
         </div>
         <div>
-          <Testimonial/>
+          <Testimonial />
         </div>
       </CommonWrapper>
     </div>
