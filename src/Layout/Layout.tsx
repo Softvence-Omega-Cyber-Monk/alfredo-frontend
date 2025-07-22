d52581406ad3ed233751ffd7cf1551a5f64fd5e7
akashsaha02 <asaha8313@gmail.com>
2025-07-22 16:54:50 +0600

    message icon button fixed

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,12 +1,11 @@
 // src/Layout/Layout.tsx
-import { Outlet, useLocation } from "react-router-dom";
-import { useTranslation } from "react-i18next"; // Import useTranslation hook
+import { Link, Outlet, useLocation } from "react-router-dom";
+import messageIcon from "@/assets/icons/message-multiple-02.svg";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Layout: React.FC = () => {
   const location = useLocation();
-  const { t } = useTranslation(); // Initialize translation hook
 
   // List of paths where Footer should be hidden
   const hideFooterPaths = [
@@ -24,11 +23,13 @@ const Layout: React.FC = () => {
         <Outlet />
       </main>
       {!shouldHideFooter && <Footer />}
-      <div>
-        {/* Example usage of translation */}
-        <p>{t("welcome")}</p> {/* Example translated text */}
-        <p>{t("description")}</p>
-      </div>
+
+      <Link
+        to="/messages"
+        className="p-2 md:p-2.5 rounded-full bg-white shadow-[0_0_10px_0_#B9D7FF]  fixed bottom-10 right-8 z-100"
+      >
+        <img src={messageIcon} className="w-4 h-4 md:w-6 md:h-6" alt="" />
+      </Link>
     </div>
   );
 };
