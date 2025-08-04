af45f0a64e2f207d48c9ed1e7a3af588fea73b76
akashsaha02 <asaha8313@gmail.com>
2025-08-04 10:01:20 +0600

    change password api integrated and also Implemented language switch in the settings page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -105,7 +105,7 @@ const routes = createBrowserRouter([
         element: <Profile />,
       },
       {
-        path: "/setting-password",
+        path: "/settings",
         element: <SettingPassword />,
       },
       {
