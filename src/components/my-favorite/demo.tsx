a0d58bef2cdb130b0902f87553444b51c2a55acf
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-09-05 10:47:30 +0600

    some update in demo component

--- src/components/my-favorite/demo.tsx
+++ src/components/my-favorite/demo.tsx
@@ -1,6 +1,12 @@
-
 const demo = () => {
-  return <div>demo</div>;
+  return (
+    <div>
+      demo Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nobis,
+      quae quos illum ex, aspernatur consectetur atque debitis eveniet quas,
+      quam delectus nemo? Pariatur, doloremque totam expedita illum excepturi
+      rem.
+    </div>
+  );
 };
 
 export default demo;
