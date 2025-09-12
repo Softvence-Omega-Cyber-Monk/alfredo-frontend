d844efa983300a2935432e1821dfcda89f6155d5
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-09-12 16:05:03 +0600

    add place modal going to fix

--- src/components/modals/AddPlaceModal.tsx
+++ src/components/modals/AddPlaceModal.tsx
@@ -3,6 +3,7 @@ import { addProperty } from "@/store/Slices/PropertySlice/propertySlice";
 import { Property } from "@/types/property";
 import { useState } from "react";
 import { toast } from "sonner";
+import { Label } from "../ui/label";
 
 interface AddPlaceModalProps {
   isOpen: boolean;
@@ -78,77 +79,98 @@ const AddPlaceModal = ({ isOpen, onClose }: AddPlaceModalProps) => {
       <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-lg">
         <h2 className="text-xl font-semibold mb-4">Add Place</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
+          <Label>Title:</Label>
           <input
             type="text"
             name="title"
             placeholder="Title"
             value={formData.title}
             onChange={handleChange}
-            className="w-full border p-2 rounded"
+            className="w-full border p-2 rounded mt-1"
             required
           />
+          <Label>Description:</Label>
           <textarea
             name="description"
             placeholder="Description"
             value={formData.description}
             onChange={handleChange}
-            className="w-full border p-2 rounded"
+            className="w-full border p-2 rounded mt-1"
             required
           />
+          <Label>Location:</Label>
           <input
             type="text"
             name="location"
             placeholder="Location"
             value={formData.location}
             onChange={handleChange}
-            className="w-full border p-2 rounded"
+            className="w-full border p-2 rounded mt-1"
             required
           />
+          <Label>Country:</Label>
           <input
             type="text"
             name="country"
             placeholder="Country"
             value={formData.country}
             onChange={handleChange}
-            className="w-full border p-2 rounded"
+            className="w-full border p-2 rounded mt-1"
             required
           />
-          <input
-            type="number"
-            name="price"
-            placeholder="Price"
-            value={formData.price}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-            required
-          />
-          <input
-            type="number"
-            name="size"
-            placeholder="Size (sqft)"
-            value={formData.size}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-            required
-          />
-          <input
-            type="number"
-            name="bedrooms"
-            placeholder="Bedrooms"
-            value={formData.bedrooms}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-            required
-          />
-          <input
-            type="number"
-            name="bathrooms"
-            placeholder="Bathrooms"
-            value={formData.bathrooms}
-            onChange={handleChange}
-            className="w-full border p-2 rounded"
-            required
-          />
+          <div className="lg:flex items-center gap-4">
+            <div className="w-1/2">
+              <Label>Price:</Label>
+              <input
+                type="number"
+                name="price"
+                placeholder="Price"
+                value={formData.price}
+                onChange={handleChange}
+                className="w-full border p-2 rounded mt-2"
+                required
+              />
+            </div>
+            <div className="w-1/2">
+              <Label>Size:</Label>
+              <input
+                type="number"
+                name="size"
+                placeholder="Size (sqft)"
+                value={formData.size}
+                onChange={handleChange}
+                className="w-full border p-2 rounded mt-2"
+                required
+              />
+            </div>
+          </div>
+          <div className="lg:flex gap-4">
+            <div className="w-1/2">
+              <Label>Bedrooms:</Label>
+              <input
+                type="number"
+                name="bedrooms"
+                placeholder="Bedrooms"
+                value={formData.bedrooms}
+                onChange={handleChange}
+                className="w-full border p-2 rounded mt-2"
+                required
+              />
+            </div>
+            <div className="w-1/2">
+              <Label>Bathrooms:</Label>
+              <input
+                type="number"
+                name="bathrooms"
+                placeholder="Bathrooms"
+                value={formData.bathrooms}
+                onChange={handleChange}
+                className="w-full border p-2 rounded mt-2"
+                required
+              />
+            </div>
+          </div>
+
           <label className="flex items-center gap-2">
             <input
               type="checkbox"
