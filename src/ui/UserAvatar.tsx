64ea113a9f5dd9b54a66f46acdfe38965d620129
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-09-23 09:47:00 +0600

    navbar popover photo changed

--- src/ui/UserAvatar.tsx
+++ src/ui/UserAvatar.tsx
@@ -1,13 +1,22 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
+import { fetchUser } from "@/store/Slices/Profile/ProfileSlice";
+import { useEffect } from "react";
 
 interface UserAvatarProps {
   userName: string;
 }
 
 const UserAvatar: React.FC<UserAvatarProps> = ({ userName }) => {
+  const dispatch = useAppDispatch();
+  const { data } = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
   return (
     <Avatar className="w-8 h-8 md:w-10 md:h-10">
-      <AvatarImage src="https://github.com/shadcn.png" />
+      <AvatarImage src={data?.photo || undefined} />
       <AvatarFallback>{userName}</AvatarFallback>
     </Avatar>
   );
