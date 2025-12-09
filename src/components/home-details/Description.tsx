3df9f7e8b32851c285718ac9c6ffb6f5b68e97e3
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-12-09 13:59:37 +0600

    home details page translation completed

--- src/components/home-details/Description.tsx
+++ src/components/home-details/Description.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "react-i18next";
+
 interface DescriptionProps {
   dates: {
     from: string;
@@ -7,17 +9,18 @@ interface DescriptionProps {
 }
 
 const Description = ({ dates, description }: DescriptionProps) => {
+  const { t } = useTranslation("homeDetails");
   return (
     <div>
       <h2 className="text-lg md:text-xl text-dark-2 font-regular mb-6 md:mb-10">
         <span className="font-semibold text-primary-blue pr-3">
-          Available From:
+          {t("availableFrom")}
         </span>
         {dates.from} to {dates.to}
       </h2>
 
       <h2 className="font-semibold text-primary-blue text-lg md:text-xl pb-1 md:pb-3 border-b border-[#BFD4F0] font-regular mb-3 md:mb-6">
-        Description
+        {t("description")}
       </h2>
       <p className="text-sm md:text-lg font-regular text-dark-3 whitespace-pre-line">
         {description || "No description available."}
