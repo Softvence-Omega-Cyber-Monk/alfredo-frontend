be43a6035f9c11ffd58736f7bb366bdfce5863da
sazzad-hosen-mahim <sajjadhosenmahim@gmail.com>
2026-02-25 19:50:43 +0600

    added gtm (google tag manager) in index.html

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -11,27 +11,25 @@ const Banner = () => {
 
   return (
     <div
-      className="w-full bg-bottom bg-repeat-x  flex flex-col items-center justify-center relative"
+      className="w-full bg-bottom bg-repeat-x flex flex-col items-center justify-center relative"
       style={{
         backgroundImage: `url('/cityscape.svg')`,
       }}
     >
       <div className="inset-0 absolute bg-[#F4F7FC] -z-20"></div>
       <CommonWrapper>
-        <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-[80px] lg:py-[140px]">
+        <div className="flex flex-col items-center justify-center gap-4 py-7 md:py-[80px] lg:py-[140px]">
           <div className="text-center font-normal text-dark-3 px-4 md:px-6">
             <h1
-              className={`capitalize max-w-[700px] mx-auto leading-[1.2] ${
-                currentLanguage === "el"
+              className={`capitalize max-w-[700px] mx-auto leading-[1.2] ${currentLanguage === "el"
                   ? "text-2xl md:text-4xl lg:text-[50px]" // smaller for Greek
                   : "text-3xl md:text-5xl lg:text-[70px]" // default for English and others
-              }`}
+                }`}
             >
               {t("banner.title")}{" "}
               <span
-                className={`font-Grand-Hotel text-primary-blue ${
-                  currentLanguage === "el" ? "" : ""
-                }`}
+                className={`font-Grand-Hotel text-primary-blue ${currentLanguage === "el" ? "" : ""
+                  }`}
               >
                 {t("banner.highlight")}
               </span>
